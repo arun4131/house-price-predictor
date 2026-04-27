27bb747804392bb5eeebb403d55cb396031e3646
apple <apple@apples-MacBook-Air.local>
2026-04-27 14:52:07 +0530

    render deployment fix

--- server.py
+++ server.py
@@ -109,4 +109,6 @@ def predict():
 # Run server
 # -------------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+import os
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
